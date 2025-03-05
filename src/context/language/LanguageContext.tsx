5420cf0856407b05d9b8664b4ea220cf6145096a
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-03-05 06:45:40 +0100

    Language context

---
+++ src/context/language/LanguageContext.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { createContext, useContext, useState, ReactNode } from "react";
+
+type LanguageContextType = {
+    language: string;
+    setLanguage: (language: string) => void;
+};
+
+const LanguageContext = createContext<LanguageContextType | undefined>(
+    undefined
+);
+
+export const LanguageProvider = ({ children }: { children: ReactNode }) => {
+    const [language, setLanguage] = useState("en");
+
+    return (
+        <LanguageContext.Provider value={{ language, setLanguage}}>
+            {children}
+        </LanguageContext.Provider>
+    );
+}
+
+export const useLanguage = () => {
+    const context = useContext(LanguageContext);
+    if (!context) {
+        throw new Error("useLanguage must be used within a LanguageProvider");
+    }
+
+    return context;
+
+}
