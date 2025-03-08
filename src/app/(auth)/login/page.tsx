63592d97ce2cd17bff12542ffff3737e6f697805
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-03-08 14:28:25 +0100

    update

--- src/app/(auth)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "@/components/auth/login-form"
-
-export default function Login() {
-    return (
-        <div className="flex items-center justify-center h-screen bg-white">
-            <LoginForm />
-        </div>
-    )
-}
---
+++ src/app/auth/page.tsx
@@ -0,0 +1,58 @@
+
+"use client"
+
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import LoginForm from "@/components/auth/login-form"
+
+export default function TabsDemo() {
+  return (
+    <Tabs defaultValue="account" className="w-[400px] flex justify-center mx-auto p-10">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="login">Login</TabsTrigger>
+        <TabsTrigger value="register">Register</TabsTrigger>
+      </TabsList>
+      <TabsContent value="login">
+        <LoginForm />
+      </TabsContent>
+      <TabsContent value="register">
+        <Card>
+          <CardHeader>
+            <CardTitle>Password</CardTitle>
+            <CardDescription>
+              Change your password here. After saving, you'll be logged out.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="current">Current password</Label>
+              <Input id="current" type="password" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="new">New password</Label>
+              <Input id="new" type="password" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save password</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs>
+  )
+}
