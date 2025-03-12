b6fec4b57b789eafcfb1a7f5aa79a59d681eb75b
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-03-12 18:24:27 +0100

    update

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,13 @@ const createJestConfig = nextJest({ dir: './' })
 
 const customJestConfig = {
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
 }
 
 module.exports = createJestConfig(customJestConfig)
