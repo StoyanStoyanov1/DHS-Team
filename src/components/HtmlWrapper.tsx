8f51f341284dbb8e7e4d6a6969e2411ba15ab60a
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-04-21 07:11:25 +0200

    update

--- src/components/HtmlWrapper.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function HtmlWrapper({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="de">
-      <body>{children}</body>
-    </html>
-  );
-}
