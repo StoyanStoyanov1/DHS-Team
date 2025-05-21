f338ce37f4ee46a641319d82ac9f59c5d4007843
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2025-05-21 13:53:07 +0200

    update

--- dhs-admin-panel/src/app/layout.tsx
+++ dhs-admin-panel/src/app/layout.tsx
@@ -1,13 +1,10 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
 import Layout from '@/src/components/Layout';
 import { AuthProvider } from '@/src/hooks/useAuth';
 import { ThemeProvider } from '@/src/components/ThemeProvider';
 // Импортът на ReduxProvider е коментиран, тъй като още няма да го използваме
 // import ReduxProvider from '@/src/components/providers/ReduxProvider';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
     title: 'Admin Dashboard',
@@ -21,7 +18,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-        <body className={inter.className}>
+        <body>
             {/* ReduxProvider е премахнат временно, тъй като още няма да се използва */}
             <ThemeProvider>
                 <AuthProvider>
